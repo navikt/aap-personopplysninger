3fc2a6262c8b76b8956f0718bdd2d3eeef6ca820
Richard Borge <richard.borge@nav.no>
2023-01-17 12:58:07 +0100

    Oppdaterer avhengigheter.
    Co-authored-by: Robin Tordly <robin.tordly@nav.no>
    Co-authored-by: Niro Karunakaran <niro.karunakaran@nav.no>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ dependencies {
     implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
 
     implementation("io.micrometer:micrometer-registry-prometheus:1.10.3")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.4")
-    runtimeOnly("ch.qos.logback:logback-classic:1.4.3")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.14.0")
+    runtimeOnly("ch.qos.logback:logback-classic:1.4.5")
     runtimeOnly("net.logstash.logback:logstash-logback-encoder:7.2")
 
     testImplementation(kotlin("test"))
