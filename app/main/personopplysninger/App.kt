5d0df163fbd75eba91fa9dd3689e81e1277b9007
Robin <robin.tordly@nav.no>
2023-03-06 14:41:03 +0100

    tombstoneable ktables
    Co-authored-by: Niro Karunakaran <niro.karunakaran@nav.no>

--- app/main/personopplysninger/App.kt
+++ app/main/personopplysninger/App.kt
@@ -13,7 +13,6 @@ import no.nav.aap.kafka.streams.v2.topology
 import no.nav.aap.ktor.config.loadConfig
 import personopplysninger.graphql.PdlGraphQLClient
 import personopplysninger.kafka.Tables
-import personopplysninger.kafka.Topics
 import personopplysninger.rest.NorgClient
 import personopplysninger.streams.aktørStream
 import personopplysninger.streams.personopplysningStream
@@ -47,13 +46,9 @@ fun Application.server(kafka: KStreams = KafkaStreams()) {
 
 internal fun topology(pdlClient: PdlGraphQLClient, norgClient: NorgClient, settOppAktørStream: Boolean) =
     topology {
-        val skjermingKTable = consume(Topics.skjerming)
-            .repartition(12)
-            .produce(Tables.skjerming)
+        val skjermingKTable = consumeRepartitioned(Tables.skjerming)
 
-        val søkereKTable = consume(Topics.søkere)
-            .map { _ -> "".toByteArray() }
-            .produce(Tables.søkere)
+        val søkereKTable = consume(Tables.søkere)
 
         if (settOppAktørStream) {
             aktørStream(søkereKTable)
